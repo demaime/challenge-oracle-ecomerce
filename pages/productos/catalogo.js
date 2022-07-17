600f400e217a82e0b431022461ce489afa96f404
demaime <aimedemian@gmail.com>
2022-07-17 18:28:51 -0300

    creado catalogo y estructura de paginas

---
+++ pages/productos/catalogo.js
@@ -0,0 +1,58 @@
+const productos = [
+  {
+    categoria: "alfajores",
+    nombre: "Alfajor de Nuez",
+    descripcion1:
+      "Masa color ámbar preparada con nueces tostadas y relleno de dulce de leche repostero",
+    descripcion2:
+      "¡Ideal para merendar con amigos o familiares, acompañado de un café negro!",
+    precio: "$120",
+    imagen: "alfajor1.jpg",
+  },
+  {
+    categoria: "alfajores",
+    nombre: "Alfajor de Chocolate",
+    descripcion1: "Masa semihúmeda de chocolate con cobertura 70% cacao",
+    descripcion2: "Recomendamos probarlo de postre, con un submarino",
+    precio: "$140",
+    imagen: "alfajor2.jpg",
+  },
+  {
+    categoria: "alfajores",
+    nombre: "Alfajor de Coco",
+    descripcion1:
+      "Masa de coco con relleno de dulce de leche repostero y coco rallado en los laterales",
+    descripcion2:
+      "¡Si sos fanático de lo extemadamente dulce, esta es tu opción!",
+    precio: "$140",
+    imagen: "alfajor3.jpg",
+  },
+  {
+    categoria: "alfajores",
+    nombre: "Alfajor de Dulce de leche",
+    descripcion1:
+      "Masa sabor dulce de leche, relleno de dulce de leche repostero. Sin cobertura",
+    descripcion2:
+      "No dejes que nadie te diga cuando es suficiente dulce de leche",
+    precio: "$120",
+    imagen: "alfajor4.jpg",
+  },
+  {
+    categoria: "alfajores",
+    nombre: "Alfajor de Mouse",
+    descripcion1:
+      "Masa sabor cacao, con abundante relleno de mouse de chocolate",
+    descripcion2: "La combinación ideal para una tarde de invierno",
+    precio: "$130",
+    imagen: "alfajor5.jpg",
+  },
+  {
+    categoria: "alfajores",
+    nombre: "Combo alfajores x3",
+    descripcion1: "Un alfajor de Coco, uno de Mouse y uno de Nuez ",
+    descripcion2:
+      "¡3 alfajores son mejores que uno! Oportunidad de degustar 3 variantes",
+    precio: "$350",
+    imagen: "alfajor6.jpg",
+  },
+];

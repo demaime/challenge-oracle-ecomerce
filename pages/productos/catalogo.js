d4aa6dd15cb8736839cb008753ab8ee4df419162
demaime <aimedemian@gmail.com>
2022-07-23 18:18:49 -0300

    agregadas funciones que fillean la info al cargar

--- pages/productos/catalogo.js
+++ pages/productos/catalogo.js
@@ -1,58 +1,58 @@
 const productos = [
   {
     categoria: "alfajores",
-    nombre: "Alfajor de Nuez",
+    nombre: "alfajor de nuez",
     descripcion1:
       "Masa color ámbar preparada con nueces tostadas y relleno de dulce de leche repostero",
     descripcion2:
       "¡Ideal para merendar con amigos o familiares, acompañado de un café negro!",
-    precio: "$120",
+    precio: "120",
     imagen: "alfajor1.JPG",
   },
   {
     categoria: "alfajores",
-    nombre: "Alfajor de Chocolate",
+    nombre: "alfajor de chocolate",
     descripcion1: "Masa semihúmeda de chocolate con cobertura 70% cacao",
     descripcion2: "Recomendamos probarlo de postre, con un submarino",
-    precio: "$140",
+    precio: "140",
     imagen: "alfajor2.JPG",
   },
   {
     categoria: "alfajores",
-    nombre: "Alfajor de Coco",
+    nombre: "alfajor de coco",
     descripcion1:
       "Masa de coco con relleno de dulce de leche repostero y coco rallado en los laterales",
     descripcion2:
       "¡Si sos fanático de lo extemadamente dulce, esta es tu opción!",
-    precio: "$140",
+    precio: "140",
     imagen: "alfajor3.JPG",
   },
   {
     categoria: "alfajores",
-    nombre: "Alfajor de Dulce de leche",
+    nombre: "alfajor de dulce de leche",
     descripcion1:
       "Masa sabor dulce de leche, relleno de dulce de leche repostero. Sin cobertura",
     descripcion2:
       "No dejes que nadie te diga cuando es suficiente dulce de leche",
-    precio: "$120",
+    precio: "120",
     imagen: "alfajor4.JPG",
   },
   {
     categoria: "alfajores",
-    nombre: "Alfajor de Mouse",
+    nombre: "alfajor de mouse",
     descripcion1:
       "Masa sabor cacao, con abundante relleno de mouse de chocolate",
     descripcion2: "La combinación ideal para una tarde de invierno",
-    precio: "$130",
+    precio: "130",
     imagen: "alfajor5.JPG",
   },
   {
     categoria: "alfajores",
-    nombre: "Combo alfajores x3",
+    nombre: "combo alfajores x3",
     descripcion1: "Un alfajor de Coco, uno de Mouse y uno de Nuez ",
     descripcion2:
       "¡3 alfajores son mejores que uno! Oportunidad de degustar 3 variantes",
-    precio: "$350",
+    precio: "350",
     imagen: "alfajor6.JPG",
   },
 ];
--- pages/productos/productos-helper.js
+++ pages/productos/productos-helper.js
@@ -1,33 +1,76 @@
 const selectedProductContainer = document.querySelector(".selected-product");
 const selectedProductInfo = document.querySelector(".product-info");
+const categoriesList = document.querySelector(".categories-list");
+const categoryContainer = document.querySelector(".category-container");
+const arrowContainer = document.getElementById("arrow-container");
+
+fillListOfProducts();
 const productListItems = Array.from(
   document.querySelectorAll(".product-list-item")
 );
 
-console.log(selectedProductContainer, selectedProductInfo, productListItems);
-
-function fillData(product) {
+function fillProductData(product) {
   const productImg = document.querySelector(".product-image");
   const productTitle = document.querySelector(".product-title");
   const productDescriptionOne = document.getElementById("product-description1");
   const productDescriptionTwo = document.getElementById("product-description2");
   const productPrice = document.getElementById("product-price");
+  const amountInput = document.querySelector(".amount-input");
+  const buttonAddToCart = document.querySelector(".add-to-cart");
+  const mensajeInicial = document.getElementById("mensaje-inicial");
 
-  productImg.src = "../../imgs/" + product.imagen;
-  productTitle.innerHTML = product.nombre;
-  productDescriptionOne.innerHTML = product.descripcion1;
-  productDescriptionTwo.innerHTML = product.descripcion2;
-  productPrice.innerHTML = product.precio;
+  selectedProductContainer.style.opacity = 0;
+  selectedProductInfo.style.opacity = 0;
+  amountInput.value = "";
+
+  setTimeout(() => {
+    productImg.src = "../../imgs/" + product.imagen;
+    productTitle.innerHTML = product.nombre.toUpperCase();
+    productDescriptionOne.innerHTML = product.descripcion1;
+    productDescriptionTwo.innerHTML = product.descripcion2;
+    productPrice.innerHTML = "##" + product.precio;
+    buttonAddToCart.disabled = false;
+    mensajeInicial.style.opacity = 0;
+  }, 350);
+  6;
+  setTimeout(() => {
+    mensajeInicial.style.display = "none";
+    selectedProductContainer.style.opacity = 1;
+    selectedProductInfo.style.opacity = 1;
+  }, 500);
 }
 
+function openCategoriesMenu() {
+  categoriesList.classList.toggle("opened");
+  categoriesList.classList.contains("opened")
+    ? (arrowContainer.innerHTML = `<i class="fa-solid fa-circle-chevron-up"></i>`)
+    : setTimeout(() => {
+        arrowContainer.innerHTML = `<i class="fa-solid fa-circle-chevron-down"></i>`;
+      }, 150);
+}
 function selectProduct(e) {
   const productClicked = productos.find(
     (product) => product.nombre === e.target.dataset.nombre
   );
-  fillData(productClicked);
-  console.log(productClicked);
+  fillProductData(productClicked);
 }
 
+function fillListOfProducts() {
+  const productList = document.querySelector(".products-list");
+  const thisCategoryProducts = productos
+    .filter((product) => product.categoria === "alfajores")
+    .map((product) => product.nombre);
+
+  thisCategoryProducts.forEach((product) => {
+    const item = document.createElement("li");
+    item.classList.add("product-list-item");
+    item.dataset.nombre = product.toLowerCase();
+    item.innerHTML = product.toUpperCase();
+    productList.appendChild(item);
+  });
+}
+
+categoryContainer.addEventListener("click", openCategoriesMenu);
 productListItems.forEach((item) =>
   item.addEventListener("click", selectProduct)
 );
